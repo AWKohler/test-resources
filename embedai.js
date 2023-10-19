79a8035d0b1d6998e281e91ef911e542940364ce
AWKohler <aronne336@icloud.com>
2023-10-19 14:09:06 -0400

    l

--- embedai.js
+++ embedai.js
@@ -2,13 +2,13 @@
 const initial_message = {"initial_message":"hey how are you?"}
 
 if (document.createStyleSheet) {
-  document.createStyleSheet("https://embedai.thesamur.ai/embedai.css?v=2");
+  document.createStyleSheet("https://test-resources.vercel.app/embedai.css?v=2");
 } else {
   var head = document.getElementsByTagName("head")[0];
   var link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
-  link.href = "https://embedai.thesamur.ai/embedai.css";
+  link.href = "https://test-resources.vercel.app/embedai.css";
   link.media = "all";
   head.appendChild(link);
 }
@@ -25,6 +25,8 @@ const htmlContent = `
   template.innerHTML = htmlContent;
   document.body.appendChild(template);
 
+
+  //embedai-Qclufpg5
   /**
    * Append the source to the Iframe.
    * embedai_id : slug of the bot
@@ -44,7 +46,7 @@ const htmlContent = `
   iconItem.src = iconSource;
 
   let initial_message_url = "https://embedai.thesamur.ai/heybotnew/get_initial_message?slug="+embedai_id;
-  
+
 
   const response = await fetch(initial_message_url);
   const data = await response.json();	
