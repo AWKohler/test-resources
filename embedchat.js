401512d8a85adc3fe84aff60db41c42ca61262d1
AWKohler <aronne336@icloud.com>
2023-12-20 22:42:51 -0500

    change

--- embedchat.js
+++ embedchat.js
@@ -1,25 +1,24 @@
-
+//TODO: Get the color from the website
 // have any questions?
-
 if (document.createStyleSheet) {
-  document.createStyleSheet("https://test-resources.vercel.app/embedai.css?v=2");
+  document.createStyleSheet("https://test-resources.vercel.app/embedchat.css?v=2");
 } else {
   var head = document.getElementsByTagName("head")[0];
   var link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
-  link.href = "https://test-resources.vercel.app/embedai.css";
+  link.href = "https://test-resources.vercel.app/embedchat.css";
   link.media = "all";
   head.appendChild(link);
 }
 
-async function init(embedai_id) {
+async function init(embedchat_id) {
 const htmlContent = `
-<div class="embedai-welcome-message" id="welcome-message"></div>
-<div class="embedai-logo" onclick="openIframe()">
-    <img class="embedai-icn" id="embedai-icon" />
+<div class="embedchat-welcome-message" id="welcome-message"></div>
+<div class="embedchat-logo" onclick="openIframe()">
+    <img class="embedchat-icn" id="embedchat-icon" />
 </div>
-<iframe class="embedai-iframe" frameBorder="0" id="embedai-iframe" style="display:none"/>
+<iframe class="embedchat-iframe" frameBorder="0" id="embedchat-iframe" style="display:none"/>
 `
 ;
 
@@ -27,42 +26,29 @@ const htmlContent = `
   template.innerHTML = htmlContent;
   document.body.appendChild(template);
 
-
-  //embedai-Qclufpg5
   /**
    * Append the source to the Iframe.
-   * embedai_id : slug of the bot
+   * embedchat_id : slug of the bot
    */
-  // let iframeSource =
-  //   "https://embedai.thesamur.ai/embedai/embed/" + embedai_id;
   let iframeSource =
-          // "https://embedai.thesamur.ai/embedai/embed/" + embedai_id;
-    // "http://localhost:3001/demo";
-    "http://localhost:3000/chat/" + embedai_id;
-    // "http://localhost:3000/chat/clqcmst850003umgxmzets2ou";
-    // "http://localhost:3000/demo/";
-    // "https://celtic-ai-saas.vercel.app/demo/";
-    // "https://celtic-ai-saas.vercel.app/chat/92";
-    // "http://localhost:3003/chat/92";
-  // let iframeSource =
-  //   "https://chathn.vercel.app/";
-  //   "https://chat.vercel.ai";
-  let widgetIframe = document.getElementById("embedai-iframe");
+    "http://localhost:3000/chat/" + embedchat_id;
+
+  let widgetIframe = document.getElementById("embedchat-iframe");
   widgetIframe.src = iframeSource;
 
   /**
    * Append the icon source to the button.
-   * embedai_id : slug of the bot
+   * embedchat_id : slug of the bot
    */
   // let iconSource =
-  //   "https://embedai.thesamur.ai/heybotnew/get_logo?slug=" + embedai_id;
+  //   "https://embedchat.thesamur.ai/heybotnew/get_logo?slug=" + embedchat_id;
   let iconSource =
     // "https://imgs.search.brave.com/o__KanFDuT4Bv1YoheAzOauIxvfMfZax7H7hFaHGtz4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC9jaGF0LWJ1/YmJsZS1pY29uLTI1/NngyNTYtNmZzdTRw/czUucG5n";
     "https://www.svgrepo.com/show/529484/chat-round-unread.svg";
-  let iconItem = document.getElementById("embedai-icon");
+  let iconItem = document.getElementById("embedchat-icon");
   iconItem.src = iconSource;
 
-  // let initial_message_url = "https://embedai.thesamur.ai/heybotnew/get_initial_message?slug="+embedai_id;
+  // let initial_message_url = "https://website/get_initial_message?slug="+embedchat_id;
 
   // const response = await fetch(initial_message_url);
   // const data = await response.json();
@@ -74,7 +60,7 @@ const htmlContent = `
 }
 
 function openIframe(){
-  let getIframe = document.querySelector('.embedai-iframe')
+  let getIframe = document.querySelector('.embedchat-iframe')
   if(getIframe.style.display=='none'){
       getIframe.style.display='block'
       window.setTimeout(function(){
@@ -84,7 +70,7 @@ function openIframe(){
       getIframe.style.setProperty('pointer-events','auto')
 
       // Change the image source to the down arrow when the iframe is open
-      let iconItem = document.getElementById('embedai-icon');
+      let iconItem = document.getElementById('embedchat-icon');
       iconItem.src = 'https://www.svgrepo.com/show/80156/down-arrow.svg';
   }else{
       getIframe.style.opacity = 0;
@@ -95,8 +81,7 @@ function openIframe(){
       getIframe.style.setProperty('pointer-events','none')
 
       // Change the image source back to the original source when the iframe is closed
-      let iconItem = document.getElementById('embedai-icon');
-      // iconItem.src = 'https://imgs.search.brave.com/o__KanFDuT4Bv1YoheAzOauIxvfMfZax7H7hFaHGtz4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC9jaGF0LWJ1/YmJsZS1pY29uLTI1/NngyNTYtNmZzdTRw/czUucG5n';
+      let iconItem = document.getElementById('embedchat-icon');
       iconItem.src = 'https://www.svgrepo.com/show/529484/chat-round-unread.svg';
   }
 
@@ -110,17 +95,6 @@ function setWelcomeMessage(data){
   welcomeMessageDiv.innerHTML = '<p>'+data.initial_message+'</p>'
 }
 
-// // var id = ""
-// var getElement = document.querySelector('script[src^="https://test-resources.vercel.app/embedai.js"]')
-// // var getElement = document.querySelector('script[src^="https://embedai.thesamur.ai/embedai.js"]')
-// // var getElement = document.querySelector('script[src^="http://localhost:5000/embedai.js"]')
-//
-// if(getElement.src.includes('?shop=')){
-//   id=getElement.src.split("=")[1]
-// }else{
-//   id=getElement.getAttribute("data-id")
-// }
-
-var id = document.getElementById("embedai").getAttribute("data-id");
+var id = document.getElementById("embedchat").getAttribute("data-id");
 
 init(id)
