ff39c8464f40963ebe65fedde476f912bb5a9776
AWKohler <aronne336@icloud.com>
2024-02-06 15:16:03 -0500

    Im gon shoot you (again)

--- embedchat.js
+++ embedchat.js
@@ -36,7 +36,8 @@ const htmlContent = `
         // Set default values if the embedchat_id is not "demo"
         // You can modify these default values accordingly
         // iframeSource = "https://embedded.botflow.io/liberty/" + embedchat_id;
-        iframeSource = "https://embedded.botflow.io/metro/" + embedchat_id;
+        // iframeSource = "https://embedded.botflow.io/metro/" + embedchat_id;
+        iframeSource = "http://localhost:3001/metro/" + embedchat_id;
 
         // iframeSource = "http://localhost:3000/liberty/" + embedchat_id;
         // iframeSource = "https://chat.vercel.ai/";
