2bd5d713daf2fce3aecc448d848c302fa84f4076
AWKohler <aronne336@icloud.com>
2024-03-07 15:10:39 -0500

    ok

--- embedchat.js
+++ embedchat.js
@@ -36,7 +36,8 @@ const htmlContent = `
         // Set default values if the embedchat_id is not "demo"
         // You can modify these default values accordingly
 
-        iframeSource = "https://embedded.botflow.io/liberty/" + embedchat_id;
+        // iframeSource = "https://embedded.botflow.io/liberty/" + embedchat_id;
+        iframeSource = "https://embedded.botflow.io/bot/" + embedchat_id;
         // iframeSource = "http://localhost:3000/bot/" + embedchat_id;
         // iframeSource = "https://embedded.botflow.io/bot/" + embedchat_id;
         // iframeSource = "http://localhost:3002/liberty/" + embedchat_id;
